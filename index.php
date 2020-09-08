82f8f55b6057fbf269a18d7cc35bad5c18a444e1
Bernard <gicehajunior76@gmail.com>
2020-09-09 00:30:57 +0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="resources/css/app.css" rel="stylesheet">
+        
+        <!-- jquery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="flex navbar">
+                <div class="flex navs">
+                    <a href="#" class="nav-link">Home</a>
+                    <a href="#" class="nav-link">Explore</a>
+                </div>
+                <div class="search-bar flex">
+                    <input type="search" name="" id="" class="search-bar-input" placeholder="">
+                    <button type="submit" class="search-button">Search</button>
+                </div>
+            </div>
+
+            <div class="card-container">
+                <div class="row">
+                    <div class="card">
+                        <h1>Weather App</h1>
+                        <p>Weather Captions</p>
+                    </div>
+                    <div class="card">
+                        <h2>Weather Icon</h2>
+                                & <br> <br>
+                        Weather Details appear here!
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="resources/js/app.js"></script>
+    </body>
+</html>
