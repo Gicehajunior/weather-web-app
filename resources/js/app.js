ece3f42d1b5fc147e421465d56c13f4cd2511f52
Bernard <gicehajunior76@gmail.com>
2020-09-11 00:56:33 +0300

    Add some web storage api functionalities(fetch)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,20 @@
 const searchBtn = document.querySelector(".search-button");
+const weatherSearchesContainer = document.querySelector(".weather-searches");
 const WeatherDisplay = document.querySelector(".weather-updates");
+const WeatherSearchTimeP = document.querySelector(".weather-status-span");
+
+const bar = document.querySelector(".bar");
+const closeSideNavBtn = document.querySelector(".close-btn");
+const SideNavbar = document.querySelector(".side-navs");
+const toggleBar = document.querySelector(".toggle-bar");
+
 let result = "";
 let weatherSearch = [];
 
 
 search();
-
+openCloseSideNavbar();
+getPreviousWeatherSearches();
 
 /*******
  * this function takes care of any weather search done. 
@@ -14,41 +23,41 @@ search();
  *      -city. 
  *      -api key for the app
  */
-function search(){
+function search(){ 
     searchBtn.addEventListener("click", () => {
         let city = $("#search-bar-input").val();
         let apiKey = $("#api-key").val();
 
         let data = {
-            q: city,
-            appid: apiKey,
-            units: "metrics",
+        q: city,
+        appid: apiKey,
+        units: "metrics",
         };
 
         $.ajax({
-            type: "GET",
-            url: "https://api.openweathermap.org/data/2.5/weather",
-            data: data,
-            dataType: "json",
+        type: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather",
+        data: data,
+        dataType: "json",
 
-            success: function (response) {
-                $.each(response.weather, (index, value) => { 
-                    result = `<p><b>${response.name}</b> <img src="public/images/${value.icon}.png" width="200" height="180"> ${response.main.temp} &deg;C ${value.main} ${value.description}<p>`;
-                    WeatherDisplay.innerHTML = result;
+        success: function (response) {
+            $.each(response.weather, (index, value) => {
+            result = `<p><b>${response.name}</b> <img src="public/images/${value.icon}.png" width="200" height="180"> ${response.main.temp}&deg;C <br> ${value.main} - ${value.description}<p>`;
+            WeatherDisplay.innerHTML = result;
 
-                    dataRetrieved = {
-                        city: response.name,
-                        icon: value.icon,
-                        temperature: response.main.temp,
-                        caption: value.main,
-                        description: value.description,
-                    };
-                    weatherSearch.push(dataRetrieved);
-                    saveWeatherSearch(weatherSearch);
-                });
-            },
+            dataRetrieved = {
+                city: response.name,
+                icon: value.icon,
+                temperature: response.main.temp,
+                caption: value.main,
+                description: value.description,
+                datetimeSearched: new Date(),
+            };
+            weatherSearch.push(dataRetrieved);
+            saveWeatherSearch(weatherSearch);
+            });
+        },
         });
-        
     });
 }
 
@@ -62,7 +71,46 @@ function saveWeatherSearch(weatherSearch){
     localStorage.setItem("weatherSearch", JSON.stringify(weatherSearch));
 }
 
+/**********
+ * this function takes care of getting the item from Web storage memory.
+ * returns the data and displays to the user.
+ */
+function getPreviousWeatherSearches(){
+    let result = "";
+    let weatherSearches = JSON.parse(localStorage.getItem("weatherSearch")); 
 
+    weatherSearches.forEach((weather) => { 
+        result = `
+                <div class="container weather-container">
+                    <h4 class="searched-region"><b>${weather.city}</b></h4>
+                    <img src="public/images/${weather.icon}.png" width="200" height="180">
+                    <p class="temp">${weather.caption} - ${weather.description}</p>
+                    <p class="temp">Temperature as of now is: ${weather.temperature}&deg;C</p>
+                </div>
+            `;
+        WeatherSearchTimeP.innerHTML = weather.datetimeSearched;
+        weatherSearchesContainer.innerHTML = result;
+    });
+    
+}
+
+
+/********
+ * this function takes care of opening and closing the side navbar. 
+ * when bar nav is clicked, side navbar opens.
+ * when close-btn nav is clicked, side navbar closes.
+ */
+function openCloseSideNavbar(){
+    bar.addEventListener('click', () => {
+        toggleBar.style.width = "50%";
+        SideNavbar.classList.add("visible");
+    });
+
+    closeSideNavBtn.addEventListener('click', () => {
+        toggleBar.style.width = "0";
+        SideNavbar.classList.remove("visible");
+    });
+}
 
 
 
